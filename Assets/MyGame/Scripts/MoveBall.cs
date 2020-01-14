3e4a12ef8f0508fe3a78e777ef5bb928a741cc21
schiderf <felix.schider02@htl-salzburg.ac.at>
2020-01-14 15:07:23 +0100

    created UI and making the first steps of the script

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    //public GameObject PosBallTopLeft;
+    //public GameObject PosBallBottomLeft;
+    //public GameObject PosBallTopRight;
+    //public GameObject PosBallBottomRight;
+
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject Sphere;
+
+    public void MoveSphere()
+    {
+        Debug.Log(Random.Range(0, 2));
+    }
+
+}
